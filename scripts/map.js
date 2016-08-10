ec3b1f447474ab2d06e124b65f3ebfd2725f4553
Joy Liu <liukang.joy@gmail.com>
2016-08-10 17:44:31 -0400

    added a map, deleted a css file

---
+++ scripts/map.js
@@ -0,0 +1,9 @@
+var mymap = L.map('mapid').setView([43.6532, -79.3832], 13); // lan&lng now is toronto; will change to query to match user's input;
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'joy9017mapbox.11h51ekk',
+    accessToken: 'pk.eyJ1Ijoiam95OTAxN21hcGJveCIsImEiOiJjaW94M2RneXQwMDJ1d2ZtNXp4a29pbTV4In0.TebEkoRfRP8E0hw_Nd3PFA'
+}).addTo(mymap);
+
+
