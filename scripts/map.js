5401641a2d384a8a9f915a88d20e13593ac520f9
Joy Liu <liukang.joy@gmail.com>
2016-08-11 10:58:06 -0400

    fixed form on submit prevent default. added promise inside form on submit. stored map loading inside function.

--- scripts/map.js
+++ scripts/map.js
@@ -1,14 +1,15 @@
-var mymap = L.map('mapid')//.setView([43.6532, -79.3832], 13); // lan&lng now is toronto; will change to query to match user's input;
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    id: 'joy9017mapbox.11h51ekk',
-    accessToken: 'pk.eyJ1Ijoiam95OTAxN21hcGJveCIsImEiOiJjaW94M2RneXQwMDJ1d2ZtNXp4a29pbTV4In0.TebEkoRfRP8E0hw_Nd3PFA'
-}).addTo(mymap);
+petApp.initMap = function() {
+	petApp.mymap = L.map('mapid')//.setView([43.6532, -79.3832], 13); // lan&lng now is toronto; will change to query to match user's input;
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+	    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+	    maxZoom: 18,
+	    id: 'joy9017mapbox.11h51ekk',
+	    accessToken: 'pk.eyJ1Ijoiam95OTAxN21hcGJveCIsImEiOiJjaW94M2RneXQwMDJ1d2ZtNXp4a29pbTV4In0.TebEkoRfRP8E0hw_Nd3PFA'
+	}).addTo(petApp.mymap);
+};
 
 petApp.displayPet = function(pets){
 	var markers = [];
-
 	petApp.shelterWithPets.forEach(function(shelterLocation){
 		// console.log(shelterLocation);
 		var latLng = L.latLng(shelterLocation.latitude.$t, shelterLocation.longitude.$t);
@@ -24,9 +25,8 @@ petApp.displayPet = function(pets){
 
 	console.log('markerGroup', markerGroup)//.getBounds())
 
-	mymap.fitBounds(markerGroup.getBounds());
-	markerGroup.addTo(mymap);
-
+	petApp.mymap.fitBounds(markerGroup.getBounds());
+	markerGroup.addTo(petApp.mymap);
 };
 
 
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -37,40 +37,39 @@ petApp.getShelter = function (query) {
 
 
 petApp.init = function () {
-	$('.searchLocation').on('submit', function(e) {
+	// console.log(locationInput);
+	$('.searchForm').on('submit', function(e) {
 	    e.preventDefault(); // default action is refreshing the page, we don't want thats
 	    petApp.locationInput = $('input[name=location]').val();
 	    $('input[name=location]').val('');
-	    // console.log(locationInput);
+	    console.log(petApp.locationInput);
 	    // getting lat lng of user location
-	});
-	// var locationInput = $('input[name=location]').val();
-	// console.log(locationInput);
-	$.when(petApp.getPet(petApp.locationInput), petApp.getShelter(petApp.locationInput)) 
+		$.when(petApp.getPet(petApp.locationInput), petApp.getShelter( petApp.locationInput)) 
 
-		.done(function(gotPet, gotShelter){
-			var pets = gotPet[0].petfinder.pets.pet;
-			var shelters = gotShelter[0].petfinder.shelters.shelter;
-			// console.log(shelters)
+			.done(function(gotPet, gotShelter){
+				var pets = gotPet[0].petfinder.pets.pet;
+				var shelters = gotShelter[0].petfinder.shelters.shelter;
+				// console.log(shelters)
 
-			shelters.forEach(function(shelterObj) {
-				var filteredPets = pets.filter(function(petObj) {
-					return petObj.shelterId.$t === shelterObj.id.$t
+				shelters.forEach(function(shelterObj) {
+					var filteredPets = pets.filter(function(petObj) {
+						return petObj.shelterId.$t === shelterObj.id.$t
+					});
+					shelterObj.pet = filteredPets;//this takes each objects out of our shelters array, creats a property called pet, whichs stores out filtered puppies
 				});
-				shelterObj.pet = filteredPets;//this takes each objects out of our shelters array, creats a property called pet, whichs stores out filtered puppies
-			});
-			petApp.shelterWithPets = shelters.filter(function(shelterObj){
-				return shelterObj.pet.length > 0
-			});
-			//go through the shelters and look at the pets
-			//if the pets array is not empty, return that shelter object into a variable
-			//variable called shelter with pets.
-			// petApp.events(); come back to this to listen for on.change events
-			petApp.displayPet(petApp.shelterWithPets);
-		}) //done
-		.fail(function(err1, err2) {
-			console.log(err1, err2)
-		}); //fail
+				petApp.shelterWithPets = shelters.filter(function(shelterObj){
+					return shelterObj.pet.length > 0
+				});
+				//go through the shelters and look at the pets
+				//if the pets array is not empty, return that shelter object into a variable
+				//variable called shelter with pets.
+				// petApp.events(); come back to this to listen for on.change events
+				petApp.displayPet(petApp.shelterWithPets);
+			}) //done
+			.fail(function(err1, err2) {
+				console.log(err1, err2)
+			}); //fail
+	});
 
 
 }; //end of init function
